ab7f53bbd4cd4d67c9d7390962bc9d8e74c97ea1
Stephen Anderson <stephenandersondev@gmail.com>
2023-08-12 22:55:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spam the Spammer
+A ruby script to spam email addresses that are spamming you. Just pass an email address in and they will be subscribed to dozens of mailing lists, some that send out multiple times daily.
+
+## Install
+1. Clone repo
+2. Run `bundle install`
+
+## Run
+Navigate to project directory, and run script while passing in the email address to be spammed:
+
+```ruby
+ruby spammer.rb <email_address>
+
+# e.g. ruby spammer.rb email@test.com
+```
---
+++ spammer.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'selenium-webdriver'
+require 'securerandom'
+
+email_address = ARGV[0]
+
+if email_address.nil?
+  puts "Email address required."
+  exit
+end
+
+puts "Preparing to spam #{email_address}"
+
+driver = Selenium::WebDriver.for :chrome
+
+# Login page
+driver.navigate.to 'https://preferences.salemwebnetwork.com/default.aspx?timeout=true'
+driver.manage.timeouts.implicit_wait=10
+driver.find_element(link_text: 'Create an account.').click
+
+# Create account
+uuid = SecureRandom.uuid.gsub('-', '_')
+user_name = email_address.split('@').first + uuid
+password = 'flood'
+
+driver.find_element(id: 'txtUsername').send_keys user_name
+driver.find_element(id: 'txtPassword').send_keys password
+driver.find_element(id: 'txtPasswordConfirm').send_keys password
+driver.find_element(id: 'MainContent_txtEmail').send_keys email_address
+driver.find_element(id: 'btnSubmit').click
+
+# Sign them up for all mailing lists
+driver.find_element(class: 'more-interests').click
+driver.find_elements(class: 'interestCheckbox').each(&:click)
+driver.find_element(id: 'btnSubmit').click
+sleep 5
